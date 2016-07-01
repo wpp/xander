d7a5b84053334d32f52a66519bee33b287498b6e
Philipp Weissensteiner <mail@philippweissensteiner.com>
2016-07-01 20:33:01 +0200

    #4 Get Xurs inventory from bungie
    This code is uuugly but I dont care.

--- ruby-app/lib/responses/xur_inventory.rb
+++ ruby-app/lib/responses/xur_inventory.rb
@@ -4,31 +4,36 @@ module Response
     TABLE = '(╯°□°）╯︵ ┻━┻'
 
     def initialize(user)
-      @url = URI('http://www.destinylfg.com/findxur/')
+      @api_key = ENV['BUNGIE_API_TOKEN'] || File.read('.bungie_token').chomp
       @user = user
+      @xur_gone = true
     end
 
     def text
-      scrape_xur_inventory
-      "Hi <@#{@user}> you asked for Xur's inventory:"
-    rescue => e
-      TABLE
+      get_xur_inventory
+      if @xur_gone
+        "Hi <@#{@user}> Xur hasn't arrived yet."
+      else
+        "Hi <@#{@user}> you asked for Xur's inventory:"
+      end
     end
 
     def attachments
+      return [] if @xur_gone
+
       fields = []
       @inventory.each do |item|
         fields << {
-          'title' => item[:text],
-          'value' => "<#{item[:link]}|Inspect>",
+          'title' => item['itemName'],
+          'value' => "<https://www.bungie.net/en/armory/Detail?item=#{item['itemHash']}|View in armory>",
           'short' => true
         }
       end
 
       [
         {
-          'fallback' => 'Xur\'s Inventory: http://www.destinylfg.com/findxur/',
-          'text' => "<http://www.destinylfg.com/findxur/|Xur's Inventory> (#{@date})",
+          'fallback' => "Xur's Inventory: #{@inventory.map {|i| i['itemName']}.join(', ')}",
+          'text' => "Xur's Inventory",
           'thumb_url' => 'https://pbs.twimg.com/profile_images/525558766180118528/hyDrezNf_400x400.png',
           'fields' => fields,
           'color' => '#FFCE1F'
@@ -37,17 +42,45 @@ module Response
     end
 
     private
-      def scrape_xur_inventory
-        doc = Nokogiri::HTML(open(@url))
+      def get(uri)
+        uri = URI(uri)
+        req = Net::HTTP::Get.new(uri)
+        req['X-API-Key'] = @api_key
+        response = Net::HTTP.start(uri.hostname, use_ssl: true) {|http|
+          http.request(req)
+        }
+        JSON.parse(response.body)
+      rescue JSON::ParserError
+        []
+      end
 
-        @date = doc.css(".col-xs-12>h2").first.children.first.text
+      def get_xur_inventory
         @inventory = []
-        doc.css(".list-unstyled").first.children.each do |c|
-          if c.name == 'li'
-            link = c.children.first.attributes['href']
-            text = c.children.first.children.text
 
-            @inventory << { text: text, link: link }
+        xur_inventory = get('https://www.bungie.net/Platform/Destiny/Advisors/Xur/')
+
+        if xur_inventory['Response'].empty?
+          return
+        else
+          @xur_gone = false
+        end
+
+        nr = Time.parse(xur_inventory['Response']['data']['nextRefreshDate'])
+        cache_name = "cache/#{nr}.json"
+        if File.exists?(cache_name)
+          @inventory = JSON.parse(File.read(cache_name))
+        else
+          sale_items = xur_inventory['Response']['data']['saleItemCategories']
+                        .map { |sic| sic['saleItems'] }
+                        .flatten
+                        .map { |s| s['item']['itemHash'] }
+
+          sale_items.each do |si|
+            item = get("https://www.bungie.net/Platform/Destiny/Manifest/6/#{si}/")
+            @inventory << item['Response']['data']['inventoryItem']
+          end
+          File.open(cache_name, 'w') do |f|
+            f.write(@inventory.to_json)
           end
         end
       end
--- ruby-app/test/responses/xur_inventory_test.rb
+++ ruby-app/test/responses/xur_inventory_test.rb
@@ -11,7 +11,15 @@ class MyEloTest < Minitest::Test
     @bot    = '<@botid>'
   end
 
-  def test_xur_inventory
+  def test_xur_inventory_when_he_is_gone
+    VCR.use_cassette('xur_inventory_gone') do
+      response = @xander.respond_to("#{@bot} xurs inventory?", @user)
+      assert_equal "Hi <@#{@user}> Xur hasn't arrived yet.", response.text
+      assert_equal [], response.attachments
+    end
+  end
+
+  def test_xur_inventory_when_he_is_there
     [
       "#{@bot} what is xur selling?",
       "#{@bot} xurs inventory?",
@@ -22,37 +30,46 @@ class MyEloTest < Minitest::Test
           response = @xander.respond_to(msg, @user)
           expected_attachment = [
             {
-              "fallback" => "Xur's Inventory: http://www.destinylfg.com/findxur/",
-              "text" => "<http://www.destinylfg.com/findxur/|Xur's Inventory> (Apr 1 – Apr 3, 2016)",
+              "fallback" => "Xur's Inventory: Plasma Drive, \"Emerald Coil\", Heavy Ammo Synthesis, Three of Coins, Glass Needles, Mote of Light, Twilight Garrison, Mask of the Third Man, Apotheosis Veil, Bad Juju, Legacy Engram",
+              "text" => "Xur's Inventory",
               "thumb_url" => "https://pbs.twimg.com/profile_images/525558766180118528/hyDrezNf_400x400.png",
-              "fields" => [
-                  {
-                    "title" => "The Taikonaut",
-                    "value" => "<http://www.destinydb.com/items/591060261-the-taikonaut|Inspect>",
-                    "short" => true
-                  },
-                  {
-                    "title" => "Sealed Ahamkara Grasps",
-                    "value" => "<http://www.destinydb.com/items/2217280775-sealed-ahamkara-grasps|Inspect>",
-                    "short" => true
-                  },
-                  {
-                    "title" => "Apotheosis Veil",
-                    "value" => "<http://www.destinydb.com/items/1519376145-apotheosis-veil|Inspect>",
-                    "short" => true
-                  },
-                  {
-                    "title" => "Exotic Gauntlet Engram",
-                    "value" => "<http://www.destinydb.com/items/111626780-exotic-engram|Inspect>",
-                    "short" => true
-                  },
-                  {
-                    "title" => "Legacy Chest Engram",
-                    "value" => "<http://www.destinydb.com/items/27147831-exotic-engram|Inspect>",
-                    "short" => true
-                  }
-              ],
-              "color" => "#FFCE1F"
+              "fields" =>
+              [
+                {"title"=>"Plasma Drive",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=1880070441|View in armory>",
+                 "short"=>true},
+                {"title"=>"\"Emerald Coil\"",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=1880070440|View in armory>",
+                 "short"=>true},
+                {"title"=>"Heavy Ammo Synthesis",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=211861343|View in armory>",
+                 "short"=>true},
+                {"title"=>"Three of Coins",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=417308266|View in armory>",
+                 "short"=>true},
+                {"title"=>"Glass Needles",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=2633085824|View in armory>",
+                 "short"=>true},
+                {"title"=>"Mote of Light",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=937555249|View in armory>",
+                 "short"=>true},
+                {"title"=>"Twilight Garrison",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=3921595523|View in armory>",
+                 "short"=>true},
+                {"title"=>"Mask of the Third Man",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=1520434779|View in armory>",
+                 "short"=>true},
+                {"title"=>"Apotheosis Veil",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=1519376145|View in armory>",
+                 "short"=>true},
+                {"title"=>"Bad Juju",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=1177550374|View in armory>",
+                 "short"=>true},
+                {"title"=>"Legacy Engram",
+                 "value"=>"<https://www.bungie.net/en/armory/Detail?item=3149602403|View in armory>",
+                 "short"=>true}
+             ],
+             "color" => "#FFCE1F"
             }
           ]
 
