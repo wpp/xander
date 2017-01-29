541118a5e83864ed8a8c8e600e7ab61d68bcfbc1
Philipp Weissensteiner <mail@philippweissensteiner.com>
2017-01-29 10:15:25 +0100

    Refactor bot

--- ruby-app/bot.rb
+++ ruby-app/bot.rb
@@ -3,37 +3,66 @@ require 'logger'
 require_relative 'lib/xander'
 require 'byebug'
 
-logger = ENV['SLACK_API_TOKEN'] ? Logger.new('log/xander.log') : Logger.new(STDOUT)
+# Bot is the first place of contact with user data.
+class Bot
+  attr_reader :logger, :client, :xander
 
-Slack.configure do |config|
-  if ENV['SLACK_API_TOKEN']
-    config.token = ENV['SLACK_API_TOKEN']
-    logger.info 'Using auth token from ENV'
-  else
-    config.token = File.read('.testbot').chomp
-    logger.info 'Using .testbot token'
+  def initialize
+    # TODO: replace this config setup with config.yml
+    Slack.config.token = ENV['SLACK_API_TOKEN'] || File.read('.testbot').chomp
+    @logger = Logger.new(ENV['SLACK_API_TOKEN'] ? 'log/xander.log' : STDOUT)
+
+    @client = Slack::RealTime::Client.new
+    @client.on(:hello, &method(:on_hello))
+    @client.on(:message, &method(:on_message))
+
+    @xander = Xander.new(@client)
+  end
+
+  def start
+    client.start!
+  end
+
+  private
+
+  def on_hello(*)
+    logger.info(
+      "Connected as #{client.self.name} to #{client.team.domain}.slack.com"
+    )
+  end
+
+  def on_message(data)
+    return unless might_respond_to?(data)
+    response = get_response_for(data)
+    respond_with(response, data) if response
+  end
+
+  # Before we decide to hand the data to xander, guard if he'd even
+  # be able to respond.
+  # TODO: find out when data doesn't have a text?
+  def might_respond_to?(data)
+    data.respond_to?(:text) && !data.text.nil? && (data.user != client.self.id)
+  end
+
+  # unify what respond_to and might_respond_to? currently are
+  # find one place where we decide to respond to a message
+  # what do we respond to? types of message vs message.text
+  # @bot messages
+  # Direct messages
+  # Someone joins bungie
+  # Someone posts a flipped table
+  def get_response_for(data)
+    xander.respond_to(data.text, data.user, data.channel, data.subtype)
+  end
+
+  def respond_with(response, data)
+    logger.info "#{data.user} message: '#{data.text}'"
+    logger.info "response: '#{response.text}'"
+    client.web_client.chat_postMessage(text: response.text,
+                                       attachments: response.attachments,
+                                       channel: data.channel,
+                                       as_user: true)
   end
 end
 
-client = Slack::RealTime::Client.new
-xander = Xander.new(client)
-
-client.on :hello do
-  logger.info "Connected '#{client.self.name}' to https://#{client.team.domain}.slack.com."
-end
-
-client.on :message do |data|
-  if data.respond_to?(:text) && !data.text.nil? && (data.user != client.self.id)
-    response = xander.respond_to(data.text, data.user, data.channel, data.subtype)
-    if response
-      logger.info "#{data.user}: #{data.text}"
-      logger.info "response: #{response.text}"
-      client.web_client.chat_postMessage(text: response.text,
-                                         attachments: response.attachments,
-                                         channel: data.channel,
-                                         as_user: true)
-    end
-  end
-end
-
-client.start!
+Bot.new.start
