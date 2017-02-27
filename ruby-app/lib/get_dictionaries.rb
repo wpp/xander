b1bcd21aa953b34195805ecb0accaee31e5a0444
Philipp Weissensteiner <mail@philippweissensteiner.com>
2017-02-27 18:07:53 +0100

    Script for retrieving dictionaries.

--- ruby-app/lib/get_dictionaries.rb
+++ ruby-app/lib/get_dictionaries.rb
@@ -13,11 +13,26 @@ end
 
 client = Slack::RealTime::Client.new
 channels = client.web_client.channels_list.channels
+groups = client.web_client.groups_list.groups
+
+count = 1000
 
 channels.each do |channel|
   puts "#{channel.name}"
-  messages = client.web_client.channels_history({ channel: channel.id, count: 1000 }).messages
+  channel_history = []
+  ts = 318297600
+  response = OpenStruct.new(has_more: true)
 
-  puts "Got: #{messages.count} messages."
-  File.open("dictionaries/#{channel.name}.txt", 'w') { |f| f.write(messages.map(&:text).join("\n")) }
+  while response.has_more
+    response = client.web_client.channels_history({ channel: channel.id, count: count, oldest: ts  })
+    # response = client.web_client.groups_history({ channel: channel.id, count: count, oldest: ts  })
+    messages = response.messages
+    channel_history += messages.reverse
+    ts = messages[0].ts
+  end
+
+  puts "History: #{channel_history.count} messages."
+  File.open("dictionaries/#{channel.name}.txt", 'w') do |f|
+    f.write(channel_history.map(&:text).join("\n"))
+  end
 end
