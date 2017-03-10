342ce18d03c9c5491f3e50d0807b81abee157074
Philipp Weissensteiner <mail@philippweissensteiner.com>
2017-03-10 19:55:30 +0100

    cleanup perk for rubocop

--- ruby-app/lib/responses/perk.rb
+++ ruby-app/lib/responses/perk.rb
@@ -1,54 +1,60 @@
 module Response
   class Perk < Base
-    def initialize(message, user, client=nil)
+    def initialize(message, user, _client = nil)
       super()
       @message = message
       @user = user
-      @db = SQLite3::Database.open('./db/world_sql_content_3393e6968b07cafc465169cf543d1bb6.content')
+      @db = SQLite3::Database.open(
+        './db/world_sql_content_3393e6968b07cafc465169cf543d1bb6.content'
+      )
       @message_query = @message.downcase.gsub(/perk/, '').lstrip
-      @text = get_text
     end
 
     def self.triggered_by?(message)
       message =~ /perk/i
     end
 
-    def get_text
-      @json = []
-      result = query
-      result.each do |r|
-        parsed = JSON.parse(r[1])
-        # for some reason some perks are duplicated
-        if @json.select { |j| j['displayName'] == parsed['displayName'] }.empty?
-          @json << parsed
-        end
-      end
+    def text
       "Hi <@#{@user}> here is what I found for: *#{@message_query}*"
     end
 
     def attachments
-      fields = []
-
-      @json.each do |item|
-        fields << {
-          'title' => item['displayName'],
-          'value' => item['displayDescription']
-        }
-      end
-
       [
         {
-          "fallback" => "Perk description for #{@message_query}",
-          "fields" => fields,
-          "color" => "#FFCE1F"
+          'fallback' => "Perk description for #{@message_query}",
+          'fields' => fields(lookup),
+          'color' => '#FFCE1F'
         }
       ]
     end
 
     private
-      def query
-        statement = "SELECT * FROM DestinySandboxPerkDefinition where json like '%#{@message_query}%' LIMIT 10"
-        result = @db.execute(statement)
+
+    def fields(db_results)
+      db_results.map do |item|
+        {
+          'title' => item['displayName'],
+          'value' => item['displayDescription']
+        }
       end
+    end
+
+    # for some reason some perks are duplicated
+    def lookup
+      json = []
+      results = @db.execute(query)
+      results.each do |r|
+        parsed = JSON.parse(r[1])
+        if json.select { |j| j['displayName'] == parsed['displayName'] }.empty?
+          json << parsed
+        end
+      end
+      json
+    end
+
+    def query
+      'SELECT * FROM DestinySandboxPerkDefinition ' \
+      "WHERE json LIKE '%#{@message_query}%' LIMIT 10"
+    end
   end
 end
