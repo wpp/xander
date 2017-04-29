5bb9041ffa5e41ecb2ec67a266575e11adb80cf9
Philipp Weissensteiner <mail@philippweissensteiner.com>
2017-04-29 20:47:09 +0200

    Memoize large dics

--- ruby-app/lib/responses/default.rb
+++ ruby-app/lib/responses/default.rb
@@ -2,12 +2,15 @@ module Response
   class Default < Base
     def initialize
       super
-      @dictionary = MarkovChain.new('lib/dictionaries/general.txt')
-      @text = @dictionary.generate_sentences(1)
+      @text = Default.dictionary.generate_sentences(1)
     end
 
     def self.triggered_by?(message)
       false
+    end
+
+    def self.dictionary
+      @@dictionary ||= MarkovChain.new('lib/dictionaries/general.txt')
     end
   end
 end
